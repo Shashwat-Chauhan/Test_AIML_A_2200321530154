92e8d4929ff8a43ce19ad3cafa9d0723a55156ba
Shashwat-Chauhan <shashwatchauhan12@gmail.com>
2024-11-07 15:48:39 +0530

    redirecting page

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,11 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
+e.preventDefault();
   const username = document.querySelector("#username").value;
   const password = document.querySelector("#password").value;
-  if (username === "admin" && password === "admin") {
-    location.href('../HTML/currency.html')
+  if (username == "admin" && password == "admin") {
+    alert("login successful")
+    window.location.href = "../HTML/currency.html"
   } else {
     alert("Login failed");
   }
